95b50c16f398eec8c842a3d49bbfd8f904df5ed1
derekmaxin <83664901+derekmaxin@users.noreply.github.com>
2021-05-19 17:34:21 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './App.css';
+import ResultComponent from './ResultComponent';
+import KeyPadComponent from "./KeyPadComponent";
+
+class App extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            result: ""
+        }
+    }
+
+    onClick = button => {
+
+        if(button === "="){
+            this.calculate()
+        }
+
+        else if(button === "C"){
+            this.reset()
+        }
+        else if(button === "CE"){
+            this.backspace()
+        }
+
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    };
+
+
+    calculate = () => {
+        var finalResult = ''
+        
+        finalResult = this.state.result
+        
+
+        try {
+            this.setState({
+                result: (eval(finalResult)) 
+            })
+        } catch (e) {
+            this.setState({
+                result: "error"
+            })
+
+        }
+		
+    };
+
+    reset = () => {
+        this.setState({
+            result: ""
+        })
+    };
+
+    backspace = () => {
+        this.setState({
+            result: this.state.result.slice(0, -1)
+        })
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="calculator-body">
+                    <h1>D's Calc!</h1>
+                    <ResultComponent result={this.state.result}/>
+                    <KeyPadComponent onClick={this.onClick}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
